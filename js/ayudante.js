8e3f87e1c126326d53266ee592b00aa1d5b74186
Edgar Camilo Figueroa Rivera <figueroacamilo@gmail.com>
2016-05-22 18:06:56 -0500

    Creo creo que aquí hay modificaciones
    Aunque maravillosamente si hay modificaciones o no eso se sabrá con etse maravilloso sistema. Giyhub.

--- js/ayudante.js
+++ js/ayudante.js
@@ -1,4 +1,20 @@
 /**
+ * Retorna un vector con palabras cuyo uso para esta práctica es representar términos prohibidos.
+ * Claro que esto para la práctica, los datos deberían provenir de una base de datos.
+ * @returns         array           retorna un vector de palabras prohibidas.
+ */
+function retornar_palabras_prohibidas()
+{
+    var arreglo = [];
+    
+    arreglo.push( "groseria" );
+    arreglo.push( "huevo" );
+    arreglo.push( "maraca" );
+    
+    return arreglo;
+}
+
+/**
  * Esta función se encarga simplemente de retornar algo adicional a una cadena que le ingresen.
  * Fué concebida para comprobar que llamar una función de este javascript se puede usar en el original o segmento del archivo html.
  * @param       string          Es una cadena ingresada, sin ánimo de operar sobre ella.
@@ -15,15 +31,22 @@ function retornar_algo( cad )
  * @param       array           vector que me permite desde el html enviar diferentes cosas.
  * @returns     string          informe con condiciones de la información
  */
-function validar_campo( cad, vector )
+function validar_campo( cad, vector, v_palabras_prohibidas )
 {
     var resultado = "";
+    var i = 0;
         
     resultado += "Longitud de la cadena: " + cad.length + " ";
-    resultado += "Si la cadena requiere validar una longitud, este sería un buen código a usar. " + ( Number( vector[ 0 ] ) );
+    resultado += "Si la cadena requiere validar una longitud, este sería un buen código a usar. ";
     
     if( cad.length < vector[ 0 ] ) resultado += "La cadena no supera la longitud suministrada.  ";
-    if( cad.length > vector[ 1 ] ) resultado += "La cadena supera la longitud suministrada.  ";   
+    if( cad.length > vector[ 1 ] ) resultado += "La cadena supera la longitud suministrada.  ";
+    
+    for( i = 0; i < v_palabras_prohibidas.length; i ++ )
+    {
+        if( cad.indexOf( v_palabras_prohibidas[ i ] + "" ) >= 0 )
+        resultado += "Hemos encontrado una palabra prohibida. [" + v_palabras_prohibidas[ i ] + "] ";
+    }
     
     return resultado; 
 }
