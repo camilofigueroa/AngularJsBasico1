ee5ccf6c9ffe2ca3131e17db2d0f7fd504538951
Edgar Camilo Figueroa Rivera <figueroacamilo@gmail.com>
2016-05-22 20:03:08 -0500

    Búsqueda AngujarJS PHP MySQL
    Aquí se implementa una búsqueda, la prueba se puede inciar con la letra a en el campo de texto, la búsqueda se hace por el alias y no por el nombre. Todo queda expuesto aquí en este repositorio para el beneficio de quien quiera aprovecharlo.

--- llamado-php.php
+++ llamado-php.php
@@ -5,30 +5,39 @@
      * incluso desde el administrador de este sitio.
      */
 
-    include( "config.php" );
-
-    /*Esta conexión se realiza para la prueba con angularjs*/
-    header("Access-Control-Allow-Origin: *");
-    header("Content-Type: application/json; charset=UTF-8");
-
-    $conn = new mysqli( $servidor, $usuario, $clave, $base_de_datos );
+    if( isset( $_GET[ 'cadena' ] ) )
+    {     
+        include( "config.php" );
+        
+        /*Esta conexión se realiza para la prueba con angularjs*/
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json; charset=UTF-8");
+        
+        $conn = new mysqli( $servidor, $usuario, $clave, $base_de_datos );
+        
+        //Se busca principalmente por alias.
+        $sql  = " SELECT * FROM tb_usuarios  ";
+        $sql .= " WHERE alias LIKE '%".$_GET[ 'cadena' ]."%'  ";
+        $sql .= " ORDER BY fecha_registro DESC LIMIT 10 ";
+        
+        //LA tabla que se cree debe tener la tabla aquí requerida, y los campos requeridos abajo.
+        $result = $conn->query( $sql );
+        
+        $outp = "";
+        
+        while($rs = $result->fetch_array( MYSQLI_ASSOC )) 
+        {
+            //Mucho cuidado con esta sintaxis, hay una gran probabilidad de fallo con cualquier elemento que falte.
+            if ($outp != "") {$outp .= ",";}
+            $outp .= '{"Alias":"'.$rs["alias"].'",';            // <-- La tabla MySQL debe tener este campo.
+            $outp .= '"Nombres":"'.$rs["nombres"].'",';         // <-- La tabla MySQL debe tener este campo.
+            $outp .= '"Apellidos":"'.$rs["apellidos"].'"}';     // <-- La tabla MySQL debe tener este campo.
+        }
+        
+        $outp ='{"records":['.$outp.']}';
+        $conn->close();
+        
+        echo($outp);
     
-    //LA tabla que se cree debe tener la tabla aquí requerida, y los campos requeridos abajo.
-    $result = $conn->query("SELECT * FROM tb_usuarios ORDER BY fecha_registro DESC LIMIT 10 ");
-
-    $outp = "";
-
-    while($rs = $result->fetch_array( MYSQLI_ASSOC )) 
-    {
-        //Mucho cuidado con esta sintaxis, hay una gran probabilidad de fallo con cualquier elemento que falte.
-        if ($outp != "") {$outp .= ",";}
-        $outp .= '{"Alias":"'.$rs["alias"].'",';            // <-- La tabla MySQL debe tener este campo.
-        $outp .= '"Nombres":"'.$rs["nombres"].'",';         // <-- La tabla MySQL debe tener este campo.
-        $outp .= '"Apellidos":"'.$rs["apellidos"].'"}';     // <-- La tabla MySQL debe tener este campo.
     }
-
-    $outp ='{"records":['.$outp.']}';
-    $conn->close();
-
-    echo($outp);
 ?> 
